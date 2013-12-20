950c4f1b76a6aa8b34b92f9e39ba869b1c443801
biaobiaoqi <shenyapeng@gmail.com>
2013-12-20 19:22:28 +0800

    Made yml as the data source of pics

--- api/api.rb
+++ api/api.rb
@@ -1,4 +1,6 @@
+#encoding=UTF-8
 require 'grape'
+require 'yaml'
 
 module Biaobiaoqi
   class API < Grape::API
@@ -10,7 +12,8 @@ module Biaobiaoqi
 
     get '/getPhotosLists' do
       content_type "text/plain"
-      {:photos => { :photo => [{:id =>"fixedgearIMG_1010.JPG", :title =>"This is a Title"}, {:id =>"fixedgearIMG_1019.JPG", :title =>"The seconde one"}]}}
+      YAML.load(File.open("./db/photos.yml"))
+      #{:photos => { :photo => [{:id =>"fixedgearIMG_1010.JPG", :title =>"This is a Title"}, {:id =>"fixedgearIMG_1019.JPG", :title =>"The seconde one"}]}}
     end
   end
 end
